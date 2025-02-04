aefc347a4061fc1ed5258156bcbdad81fd542fa2
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-04 20:38:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// Classe que representa um catálogo de jogos, contendo uma lista de jogos e métodos para manipulá-la.
+CatalogoJogos catalogoJogos = new();
+
+catalogoJogos.AdicionarJogo("The Witcher 3: Wild Hunt", "RPG", 2015);
+catalogoJogos.ListarJogos();
